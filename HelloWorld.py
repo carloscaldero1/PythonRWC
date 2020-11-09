5915220701b79dfeb51b8643ae81f41a868a8de4
carloscaldero1 <70708115+carloscaldero1@users.noreply.github.com>
2020-11-09 13:47:58 -0500

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+#This is a comment, it is ingored by the complier so you can write anything here
+#Always comment your work
+''' 
+This
+is how you comment
+a whole block of code
+without putting # on every single line
+This is Very Useful
+'''
+#The output after I run this code should be: Hello World 
+print ("Hello World")    #to the left is a print statement, anything after the # is ignored
+
+#Save as a .py file and run to see output
